5ded789620a6c19a82f7ffc15122182b81f87c31
emre <turanemre2600@gmail.com>
2024-07-13 03:28:53 +0300

    init

---
+++ src/main/java/org/selfproject/soap_api/endpoint/CountryEndpoint.java
@@ -0,0 +1,32 @@
+package org.selfproject.soap_api.endpoint;
+
+import org.selfproject.soap_api.repository.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import jaxb.io.spring.guides.gs_producing_web_service.GetCountryRequest;
+import jaxb.io.spring.guides.gs_producing_web_service.GetCountryResponse;
+
+@Endpoint
+public class CountryEndpoint {
+    private static final String NAMESPACE_URI = "http://spring.io/guides/gs-producing-web-service";
+
+    private CountryRepository countryRepository;
+
+    @Autowired
+    public CountryEndpoint(CountryRepository countryRepository) {
+        this.countryRepository = countryRepository;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
+    @ResponsePayload
+    public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
+        GetCountryResponse response = new GetCountryResponse();
+        response.setCountry(countryRepository.findCountry(request.getName()));
+
+        return response;
+    }
+}
